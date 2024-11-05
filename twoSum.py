b7441d03438898fc5f73296599de710667fae165
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2024-11-05 23:47:07 +0600

    1. Two Sum problem solution

--- twoSum.py
+++ twoSum.py
@@ -1,1 +1,11 @@
-print(2+2)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        store = {}
+
+        for i in range(len(nums)):
+            if nums[i] in store:
+                diffIndex = store[nums[i]]
+                return [diffIndex,i]
+            else:
+                diffIndex2 = target - nums[i]
+                store[diffIndex2] = i
